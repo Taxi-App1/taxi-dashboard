ef4e45158c8f4959a95a658c534fff035c97e34a
Ahmad  Kharma <ahmad2003kharma@gmail.com>
2023-11-06 21:12:32 +0200

    dashboard

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import DashboardCard from "../Components/DashboardComponent/dashboardCard";
-import DonateCharts from "../Components/DashboardComponent/dashboardChart";
+// import DonateCharts from "../Components/DashboardComponent/dashboardChart";
 function DashboardPage () {
     return ( <div className="m-16 flex flex-col md:gap-[100px]">
         <DashboardCard/>
 
-<div className="flex flex-row justify-between">
+{/* <div className="flex flex-row justify-between">
         <DonateCharts/>
         <DonateCharts/>
-        </div>
+        </div> */}
     </div> );
 }
 
