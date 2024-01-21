2327fa4a5dd74071b5482edaf344a80f1d5a1f18
khahmad1 <ahmad2003kharma@gmail.com>
2024-01-21 14:12:17 +0200

    fix the side bar

--- src/Components/sideBarComponent.jsx
+++ src/Components/sideBarComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Import Link from React Router
 import logo from "../assets/logo.jpg"
 import car from "../assets/car.svg"
@@ -10,6 +10,7 @@ import logout from "../assets/logout.svg"
 
 
 function SideBarComponent(props) {
+  const [clicked,setcliked]=useState(true)
   const links = [
     {
       name: "Dashboard",
@@ -41,24 +42,24 @@ function SideBarComponent(props) {
   let { className } = props;
 
   return (
-    <div className={`flex flex-col h-screen items-center p-10 justify-around bg-white ${className}`}>
+    <div className={`flex flex-col h-screen shadow-lg items-center p-10 justify-around bg-white ${className}`}>
       <div>
         <img src={logo} alt="Logo" className="rounded-full md:w-32 md:h-32 w-full h-full"/>
       </div>
-      <div className="flex flex-col gap-4">
-        {links.map((link, index) => (<div className="flex flex-row   md:justify-between gap-4" key={index}>
-        <img src={link.icon} alt="Logo" className=" self-center w-10 h-10"/>
-          <Link className=" text-secondary font-semibold text-3xl  md:w-32 py-5 " key={index} to={link.link}>
+      <div className="flex  flex-col gap-4">
+        {links.map((link, index) => (<div  className={`flex flex-row justify-around px-2  bg-gradient-to-r from-secondary to-primary   rounded-lg  md:justify-between gap-4`} key={index}>
+        <img src={link.icon} alt="Logo" className= {` bg-white  rounded-lg  p-2  self-center w-10 h-10`}/>
+          <Link className={`${clicked ?"text-white ":"text-secondary"}   font-semibold text-2xl  flex-1  py-3 `} key={index} to={link.link}>
             {link.name}
           </Link>
           </div>
         ))}
         </div>
         
-        <div className="flex flex-row border-secondary  md:justify-between gap-4">
-        <img src={logout} alt="Logo" className="  self-center w-10 h-10"/>
-          <p className=" text-secondary font-semibold  text-3xl w-32 py-5 " >
-            Admin
+        <div className={`flex flex-row justify-around px-2  bg-gradient-to-r from-secondary to-primary   rounded-lg  md:justify-between gap-4`} >
+        <img src={logout} alt="Logo" className= {` bg-white  rounded-lg  p-2  self-center w-10 h-10`}/>
+          <p className={` text-white   font-semibold text-2xl w-32  flex-1  py-3 `} >
+          Admin
           </p>
           </div>
     </div>
